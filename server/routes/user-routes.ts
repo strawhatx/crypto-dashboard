7b353d8e0b3f37c9ee7e60356014b6eaa9a61408
Nathaniel James <nathanieltjames24@gmail.com>
2022-01-27 02:58:55 -0500

    got rid of the errors

--- server/routes/user-routes.ts
+++ server/routes/user-routes.ts
@@ -1,15 +1,16 @@
 import { Router } from "express";
-import { CheckAuth } from "../middleware/checkAuth";
+//import { CheckAuth } from "../middleware/checkAuth";
 import { UserController } from "../controllers/user-controller";
 
 const router = Router();
 const routes = new UserController();
 
-router.get("/", CheckAuth, routes.getUsers);
-router.get("/:id", CheckAuth, routes.getUserById);
-router.put("/", CheckAuth, routes.updateUser);
-router.post("/", CheckAuth, routes.createUser);
-router.delete("/:id", CheckAuth, routes.deleteUser);
+//router.get("/", CheckAuth, routes.getUsers);
+router.get("/", routes.getUsers);
+router.get("/:id", routes.getUserById);
+router.put("/", routes.updateUser);
+router.post("/", routes.createUser);
+router.delete("/:id", routes.deleteUser);
 
 export const UserRoutes = router;
 
